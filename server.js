3442a8f90b76897a291f7e7a2e1d64663754c180
borridge <boristadic322@hotmail.com>
2025-07-16 11:32:19 +1000

    adding note

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('Hello from Dockerized Node.js!'));
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+
+// 
