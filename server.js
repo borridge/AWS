5249e7f42db38995e5ab17a00a09344ae189ab35
borridge <boristadic322@hotmail.com>
2025-07-16 12:11:08 +1000

    env addition

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+require('dotenv').config();
 app.get('/', (req, res) => res.send('Hello from Dockerized Node.js!'));
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
